83c1d53df82e32e5244c653450509d92118706fe
Khoa Tran <trandangkhoa@tma.com.vn>
2017-03-02 12:47:29 +0700

    Add more response header to cors filter

--- backend/src/main/java/khoa/training/task/tracking/configuration/MyCORSFilter.java
+++ backend/src/main/java/khoa/training/task/tracking/configuration/MyCORSFilter.java
@@ -23,6 +23,9 @@ public class MyCORSFilter implements Filter {
         {
             HttpServletResponse response = (HttpServletResponse) servletResponse;
             response.addHeader("Access-Control-Allow-Origin","*");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+            response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         }
         filterChain.doFilter(servletRequest,servletResponse);
     }
